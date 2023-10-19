0bc01c73e16068e3ec85769841b601ebf6cf86fe
audi1218 <114365769+audi1218@users.noreply.github.com>
2023-10-19 15:07:58 -0400

    Add files via upload

---
+++ VideoGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using VideoGameLab;
+
+string projectRootFolder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
+
+string filePath = projectRootFolder + "/videogames.csv";
+
+List<VideoGame> Games = new List<VideoGame>();
+
+using (var sr = new StreamReader(filePath))
+{
+    sr.ReadLine();
+
+    while (!sr.EndOfStream)
+    {
+        string line = sr.ReadLine();
+
+        string[] lineData = line.Split(',');
+
+         VideoGame game = new VideoGame();
+
+        game.Name = lineData[0];
+        game.Platform = lineData[1];
+        game.Year = int.Parse(lineData[2]);
+        game.Genre= lineData[3];
+        game.Publisher = lineData[4];
+        game.NASales = double.Parse(lineData[5]);
+        game.EUSales = double.Parse(lineData[6]);   
+        game.JPSales = double.Parse(lineData[7]);
+        game.OtherSales = double.Parse(lineData[8]);
+        game.GlobalSales = double.Parse(lineData[9]);
+
+        Games.Add(game);
+
+       
+    }
+}
---
+++ VideoGame/VideoGame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// video game class
+namespace VideoGameLab
+{
+    public  class VideoGame : IComparable<VideoGame>
+    {
+        //properties
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public int Year {  get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public double NASales { get; set; }
+        public double EUSales { get; set; }
+        public double JPSales { get; set; }
+        public double OtherSales { get; set; }
+        public double GlobalSales { get; set; }
+
+        public int CompareTo(VideoGame other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Platform: {Platform}, Year: {Year}, Genre: {Genre}, Publisher: {Publisher}, NA Sales: {NASales}, EU Sales: {EUSales}, JP Sales: {JPSales}, OtherSales: {OtherSales}, Global Sales: {GlobalSales}";
+        }
+    }
+}
